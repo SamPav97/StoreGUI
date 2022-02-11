e254d5c28549fe5fe212ceee58dde977575a042c
Samuil <samuil.pavlevchev@gmail.com>
2022-02-11 17:25:01 +0000

    Almost done

--- auth_service.py
+++ auth_service.py
@@ -1,5 +1,6 @@
 import json
 
+USERS_FILE_PATH = './db/users.txt'
 
 def register(username, email, password):
     user_obj = {
@@ -9,9 +10,19 @@ def register(username, email, password):
     }
 
     user_json = json.dumps(user_obj)
-    with open('./db/users.txt', 'r+') as file:
+    with open(USERS_FILE_PATH, 'r+') as file:
         for user_line in file:
             existing_user = json.loads(user_line.strip())
-        if existing_user['username'] == username:
-            return False
+            if existing_user['username'] == username:
+                return False
+        file.write(user_json + "\n")
+        return True
 
+
+def login(username, password):
+    with open(USERS_FILE_PATH, 'r') as file:
+        for user_line in file:
+            existing_user = json.loads(user_line.strip())
+            if existing_user['username'] == username and existing_user['password'] == password:
+                return True
+        return False
---
+++ products_service.py
@@ -0,0 +1,9 @@
+import json
+
+
+def get_all_products():
+    with open('./db/products.txt', 'r') as file:
+        return [json.loads(x.strip()) for x in file]
+
+
+
